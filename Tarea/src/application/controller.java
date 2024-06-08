e3f010afc29843bf2fe543a80825f6654881c4de
CentenoTapia <168596867+CentenoTapia@users.noreply.github.com>
2024-06-08 06:15:02 -0500

    Add files via upload

---
+++ Tarea/src/application/controller.java
@@ -0,0 +1,26 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class controller {
+	
+	@FXML private Button button;
+    @FXML private CheckBox checkBox;
+    @FXML private Hyperlink hyperlink;
+    @FXML private ToggleButton toggleButton;
+    @FXML private RadioButton radioButton;
+    @FXML private Label label;
+    @FXML private TextField textField;
+    @FXML private PasswordField passwordField;
+    @FXML private TextArea textArea;
+    @FXML private ProgressIndicator progressIndicator;
+    @FXML private ProgressBar progressBar;
+    @FXML private Slider slider;
+
+    @FXML
+    public void initialize() {
+        
+    }
+
+}
